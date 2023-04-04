d2f03a54192a175b63f118fbb1c89a8ee44c164a
sairevanth <sairevanth999@gmail.com>
2023-04-04 20:35:25 +0530

    update

--- terraform_files/k8s.tf
+++ terraform_files/k8s.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "demoserver" {
  ami = "ami-007855ac798b5175e"
  instance_type = "t2.medium"
  vpc_security_group_ids = [aws_security_group.my_sg.id]
- key_name = "KeyPair"
+ key_name = "DEMOKEY"
    root_block_device {
       volume_size = 20
       volume_type = "gp2"
@@ -20,8 +20,7 @@ resource "aws_instance" "demoserver" {
  "sudo wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64",
  "sudo chmod +x /home/ubuntu/minikube-linux-amd64",
  "sudo cp minikube-linux-amd64 /usr/local/bin/minikube",
- "curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s 
-https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl",
+ "curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl",
  "sudo chmod +x /home/ubuntu/kubectl",
  "sudo cp kubectl /usr/local/bin/kubectl",
  "sudo groupadd docker",
@@ -31,7 +30,7 @@ https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/
  type = "ssh"
  host = self.public_ip
  user = "ubuntu"
- private_key = file("KeyPair.pem")
+ private_key = file("./DEMOKEY.pem")
  }
  }
 }
